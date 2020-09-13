17a60269773a2172bab7cc811b6194867da19323
Santiago Alvarado <s.alvarado0@icloud.com>
2020-09-13 20:47:57 -0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public List<Usuario> Test()
+        {
+            using (var context = new BugTrackerFinalEntities())
+            {
+                var query = context.Usuarios.ToList();
+                return query;
+            }
+        }
+            
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 c = new Class1();
+            c.Test();
+        }
+    }
+}
